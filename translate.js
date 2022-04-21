c22037450ec9e92e3f33807cb2ab66340bd0c22d
Mandy Parker <amandamgranger@gmail.com>
2022-04-21 14:44:03 -0400

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,6 +1,7 @@
 //translate.js 
 // This file is used to rewrite the urls in the language selector to take users to that page on the translated site.
-
+let siteLang = document.documentElement.lang;
+if (siteLang === "en") {
 let a = window.location.href.substring(window.location.protocol.length + 2);
 let items = document.querySelectorAll("ul#select-language li a");
 
@@ -12,5 +13,17 @@ items.forEach((item) => {
 		item.href = "https://" + d + "." + a;
 	}
 });
+} else {
+	let a = window.location.href.substring(window.location.protocol.length + 5);
+	let items = document.querySelectorAll("ul#select-language li a");
 
+items.forEach((item) => {
+	let d = item.lang;
+	if (d === "en") {
+		item.href = "https://" + a;
+	} else {
+		item.href = "https://" + d + "." + a;
+	}
+});
+}
 
