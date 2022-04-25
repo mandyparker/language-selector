f1516f445407d0266e099ce9468a456177088bba
Mandy Parker <amandamgranger@gmail.com>
2022-04-25 11:04:48 -0400

    Version 2
    Simplified the code a bit. Now, we just grab the href value of our footer links, which contains the base url of our english and alternate language sites, and append our current path to that URL. Also includes a URL sanitization library to prevent xss attacks.

--- translate.js
+++ translate.js
@@ -1,29 +1,27 @@
-//translate.js 
+//translatev2.js 
 // This file is used to rewrite the urls in the language selector to take users to that page on the translated site.
-let siteLang = document.documentElement.lang;
-if (siteLang === "en") {
-let a = window.location.href.substring(window.location.protocol.length + 2);
+
+/**
+ * Sanitize a URL.
+ *
+ * Source @braintree/sanitize-url
+ * <https://github.com/braintree/sanitize-url>
+ * License: MIT(https://github.com/braintree/sanitize-url/blob/main/LICENSE)
+ * @param {string} url
+ * @return {string}
+ */
+function sanitizeUrl(url){if(!url){return"about:blank"}
+var invalidProtocolRegex=/^(%20|\s)*(javascript|data|vbscript)/im;var ctrlCharactersRegex=/[^\x20-\x7EÀ-ž]/gim;var urlSchemeRegex=/^([^:]+):/gm;var relativeFirstCharacters=[".","/"];function _isRelativeUrlWithoutProtocol(url){return relativeFirstCharacters.indexOf(url[0])>-1}
+var sanitizedUrl=url.replace(ctrlCharactersRegex,"").trim();if(_isRelativeUrlWithoutProtocol(sanitizedUrl)){return sanitizedUrl}
+var urlSchemeParseResults=sanitizedUrl.match(urlSchemeRegex);if(!urlSchemeParseResults){return sanitizedUrl}
+var urlScheme=urlSchemeParseResults[0];if(invalidProtocolRegex.test(urlScheme)){return"about:blank"}
+return sanitizedUrl}
+
 let items = document.querySelectorAll("ul#select-language li a");
-
+let a = window.location.href.replace(window.location.origin, "").substring(1);
 items.forEach((item) => {
-	let d = item.lang;
-	if (d === "en") {
-		item.href = window.location.href;
-	} else {
-		item.href = "https://" + d + "." + a;
-	}
-});
-} else {
-	let a = window.location.href.substring(window.location.protocol.length + 5);
-	let items = document.querySelectorAll("ul#select-language li a");
+	let b = item.href;
 
-items.forEach((item) => {
-	let d = item.lang;
-	if (d === "en") {
-		item.href = "https://" + a;
-	} else {
-		item.href = "https://" + d + "." + a;
-	}
-});
-}
+		item.href = sanitizeUrl(b + a);
 
+});
